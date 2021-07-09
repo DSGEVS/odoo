6fbc91ce8bcb6cdbd5085e9822744c9ddb856372
GSD DEVELOPERS GROUP <55499071+DSGEVS@users.noreply.github.com>
2021-07-09 18:38:57 -0500

    add stock card

---
+++ gsd_operations_stock_available/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "gsd_purchase_stock_available",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'purchase',
+        'sale_management',
+        'stock',
+    ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/purchase_order_stock.xml',
+        'views/sale_order_stock.xml',
+        'views/stock_picking.xml'
+    ],
+}
---
+++ gsd_operations_stock_available/models/stock_picking.py
@@ -0,0 +1,6 @@
+
+from odoo import api, fields, models
+
+class StockPicking(models.Model):
+    _inherit = 'stock.move'
+    stock_available = fields.Float(string='On hand', related='product_id.qty_available')
