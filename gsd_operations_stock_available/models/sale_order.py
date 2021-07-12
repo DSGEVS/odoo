45eb9d8015c7a693ad969ea52e9749ab30e2af53
GSD DEVELOPERS GROUP <55499071+DSGEVS@users.noreply.github.com>
2021-07-12 15:47:16 -0500

    add stock card

---
+++ gsd_operations_stock_available/models/sale_order.py
@@ -0,0 +1,6 @@
+
+from odoo import api, fields, models
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+    stock_available = fields.Float(string='On hand', related='product_id.qty_available')
