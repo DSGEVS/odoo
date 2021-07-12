52479f77773c4d1840f8cd87cfc10a05d62af5e5
GSD DEVELOPERS GROUP <55499071+DSGEVS@users.noreply.github.com>
2021-07-12 16:18:21 -0500

    add stock card

---
+++ gsd_operations_stock_available/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "View products available stock in Sales, Purchases and transfers",
+
+    'summary': """
+        Adds new field in Sale Order Line, Purchase Order Line and Transfer Stock Line to grab the stock.
+        """,
+
+    'description': """
+    
+    """,
+
+    'author': "GSD intelligence - David Lizarraga Corne",
+    'category': 'Warehouse',
+    'license': 'AGPL-3',
+    'version': '13.0.1',
+    # any module necessary for this one to work correctly
+    'depends': [
+        'purchase',
+        'sale_management',
+        'stock',
+    ],
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/purchase_order_stock.xml',
+        'views/sale_order_stock.xml',
+        'views/stock_picking.xml'
+    ],
+    'images': ['static/description/banner.png'],
+    'price': 8,
+    'currency': 'USD',
+}
