98581bd7ffe102a13cdc833c8739f44e9e6d76b0
GSD DEVELOPERS GROUP <55499071+DSGEVS@users.noreply.github.com>
2021-07-12 12:25:35 -0500

    add stock card

--- gsd_operations_stock_available/__manifest__.py
+++ gsd_operations_stock_available/__manifest__.py
@@ -12,14 +12,9 @@
     'license': 'AGPL-3',
     'version': '11.0.1',
     # any module necessary for this one to work correctly
-    'depends': [
-        'purchase',
-        'sale_management',
-        'stock',
-    ],
+    'depends': ['purchase', 'sale_management', 'stock'],
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/purchase_order_stock.xml',
         'views/sale_order_stock.xml',
         'views/stock_picking.xml'
