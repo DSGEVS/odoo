da065450451eb704a1845d5fbb75aa46c23207e7
GSD DEVELOPERS GROUP <55499071+DSGEVS@users.noreply.github.com>
2021-09-09 14:56:55 -0500

    add new app

---
+++ gsd_bom_sale_order_mrp/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "gsd_bom_sale_order_mrp",
+
+    'summary': """
+        Select BOM in your sale order and show the components in your quotation 
+        """,
+
+    'description': """
+        Add BOM to Sale order line and show its components in description field and quotation. Also you can view the Sale order in your Manufacture order
+    """,
+
+    'author': "GSD Intellegence",
+    'category': 'Sales',
+    'version': '11.0.1',
+    'license': 'AGPL-3',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale_management'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/sale_order.xml',
+        'views/mrp_production.xml',
+    ],
+    # only loaded in demonstration mode
+    'images': ['static/description/banner.PNG'],
+    'price': 20,
+    'currency': 'EUR',
+}
+
+
+
