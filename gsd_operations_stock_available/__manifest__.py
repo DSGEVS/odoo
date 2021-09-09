5c5cbcc527a42300232681feb6144c153f5a623d
GSD DEVELOPERS GROUP <55499071+DSGEVS@users.noreply.github.com>
2021-09-09 15:51:54 -0500

    [UPDATE]

--- gsd_operations_stock_available/__manifest__.py
+++ gsd_operations_stock_available/__manifest__.py
@@ -10,7 +10,7 @@
     
     """,
 
-    'author': "GSD intelligence - David Lizarraga Corne",
+    'author': "GSD intelligence",
     'category': 'Warehouse',
     'license': 'AGPL-3',
     'version': '11.0.1',
