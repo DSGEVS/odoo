fb2ec37fec060cdd0aa0431c88039020c8a08016
GSD DEVELOPERS GROUP <55499071+DSGEVS@users.noreply.github.com>
2021-09-10 18:08:20 -0500

    [UPDATE]

--- gsd_bom_sale_order_mrp/__manifest__.py
+++ gsd_bom_sale_order_mrp/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "gsd_bom_sale_order_mrp",
+    'name': "Add BoM in sale order line",
 
     'summary': """
         Select BOM in your sale order and show the components in your quotation 
